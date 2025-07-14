a46e2c3f3f13066cd425a7e853023e965bef3fbe
Angela Qian <angelazqian@gmail.com>
2025-07-14 11:49:37 -0700

    add finetuned model

--- js/application.js
+++ js/application.js
@@ -1,7 +1,9 @@
 let Imitationmodel;
+let Finemodel;
 // let RLmodel;
 // const reinforcementCheckbox = document.querySelector(".rl-button");
 const imitationCheckbox = document.querySelector(".imitation-button");
+const fineCheckbox = document.querySelector(".fine-button");
 const strategyCheckbox = document.querySelector(".strategy-button");
 const speedButtons = {
   fast: document.querySelector(".full-speed-button"),
@@ -13,6 +15,10 @@ async function loadImitationModel() {
   Imitationmodel = await tf.loadGraphModel('imitation-learn/2048_imitation_tfjs/model.json');
   console.log("Imitation model loaded");
 }
+async function loadFineModel() {
+  Finemodel = await tf.loadGraphModel('full-model/2048_fine_tfjs/model.json');
+  console.log("Finetuned model loaded");
+}
 // async function loadRLModel() {
 //   RLmodel = await tf.loadGraphModel('reinforcement-learn/2048_rl_tfjs/model.json');
 //   console.log("Reinforcement model loaded");
@@ -23,6 +29,14 @@ async function predictImitationMove(grid) {
   const log2grid = grid.cells.map(row =>row.map(cell => (cell ? Math.log2(cell.value) : 0)));
   const inputTensor = tf.tensor(log2grid, [4, 4], 'float32').reshape([1, 1, 4, 4]); //reshape for cnn
   const prediction = Imitationmodel.predict(inputTensor);
+  const probs = await prediction.data();
+  const rankedMoves = [...probs.keys()].sort((a, b) => probs[b] - probs[a]);  //spread iterable, sort moves by probability
+  return rankedMoves; // 0: up, 1: right, 2: down, 3: left
+}
+async function predictFineMove(grid) {
+  const log2grid = grid.cells.map(row =>row.map(cell => (cell ? Math.log2(cell.value) : 0)));
+  const inputTensor = tf.tensor(log2grid, [4, 4], 'float32').reshape([1, 1, 4, 4]); //reshape for cnn
+  const prediction = Finemodel.predict(inputTensor);
   const probs = await prediction.data();
   const rankedMoves = [...probs.keys()].sort((a, b) => probs[b] - probs[a]);  //spread iterable, sort moves by probability
   return rankedMoves; // 0: up, 1: right, 2: down, 3: left
@@ -56,6 +70,28 @@ async function autoImitationPlay(gameManager) {
       delay = 500;
     }
     setTimeout(() => autoImitationPlay(gameManager), delay); //repeat
+  }
+}
+async function autoFinePlay(gameManager) {
+  if (!fineCheckbox.checked) return;
+
+  if (!gameManager.isGameTerminated()) {
+    const originalGridState = JSON.stringify(gameManager.grid.cells); // save current grid state
+    const rankedmoves = await predictFineMove(gameManager.grid);
+    for (let move of rankedmoves) {
+      gameManager.move(move);
+      if (JSON.stringify(gameManager.grid.cells) !== originalGridState) {
+        break;
+      }
+      console.log("model got stuck lmao");
+    }
+    let delay = 200; //speed checks
+    if (speedButtons.fast.checked) {
+      delay = 0;
+    } else if (speedButtons.slow.checked) {
+      delay = 500;
+    }
+    setTimeout(() => autoFinePlay(gameManager), delay); //repeat
   }
 }
 
@@ -110,6 +146,15 @@ window.requestAnimationFrame(() => {
         await loadImitationModel(); // ensure model is loaded
       }
       autoImitationPlay(gameManager);
+    }
+  });
+
+  fineCheckbox.addEventListener("change", async () => {
+    if (fineCheckbox.checked) {
+      if (!Finemodel) {
+        await loadFineModel(); // ensure model is loaded
+      }
+      autoFinePlay(gameManager);
     }
   });
   // reinforcementCheckbox.addEventListener("change", async () => {
