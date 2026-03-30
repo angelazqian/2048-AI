4656694e8a984507ee3c8521ee4763224fd20fd6
Angela Qian <angelazqian@gmail.com>
2026-03-30 14:05:25 -0400

    i have made a severe and continuous lapse in my judgement

--- js/local_storage_manager.js
+++ js/local_storage_manager.js
@@ -19,8 +19,8 @@ window.fakeStorage = {
 };
 
 function LocalStorageManager() {
-  this.bestScoreKey     = "bestScore";
-  this.gameStateKey     = "gameState";
+  this.bestScoreKey     = "2048ai-bestScore";
+  this.gameStateKey     = "2048ai-gameState";
 
   var supported = this.localStorageSupported();
   this.storage = supported ? window.localStorage : window.fakeStorage;
